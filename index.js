5c19174157caeb9945f06cf4fc74ce1f535a670c
Josh <joshuakettle1990@gmail.com>
2017-10-25 20:42:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(n, katzDeliLine, name){
-    katzDeli.push(name)  
+    katzDeli.push(name)
   return 'Welcome, ' + name + '. you are number ' + [n + 1] + ' in line.';
 }
-
