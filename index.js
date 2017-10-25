d77f24121732cf101534bbc03f7512b839b1ee9a
Josh <joshuakettle1990@gmail.com>
2017-10-25 21:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nowServing(katzDeli){
   if (!katzDeli.length){
     return "There is nobody waiting to be served!";
   }else{
-    return "Currently Serving " + katzDeli.unshift() + ". ";
+    return `Currently Serving  ${katzDeli.unshift()} . `;
   }
 }
