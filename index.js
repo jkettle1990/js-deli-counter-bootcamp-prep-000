3474bda5515568472db7c29ace339fd9749b4c4a
Josh <joshuakettle1990@gmail.com>
2017-10-25 20:49:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeli, name){
     katzDeli.push(name)
   return 'Welcome, ' + name + '. You are number ' + katzDeli.length + ' in line.';
 }
+
+function nowServing(){
+  if (katzDeli.length > 1){
+    return katzDeli[0];
+    katzDeli.shift();
+  }else if (katzDeli.length == 0){
+    return "There is nobody waiting to be served!"
+  }
+}
